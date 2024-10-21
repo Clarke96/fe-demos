54075afdd0c3d3a4c4ca1dd999be126f64b004e9
Sean <stmclarke@gmail.com>
2024-10-21 18:55:23 +0100

    feat: added logo and service script. export-xlsx

--- angular/apps/xlsx-export/src/services/export-xlsx.service.ts
+++ angular/apps/xlsx-export/src/services/export-xlsx.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@angular/core';
+import * as ExcelJS from 'exceljs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ExportXlsxService {
   download() {
-    console.log('Download xlsx');
+    const workbook = new ExcelJS.Workbook();
+    const albums = workbook.addWorksheet('Albums');
+    const cover = workbook.addWorksheet('Cover');
+
+    const logo = workbook.addImage({
+      filename: '../assets/images/logo.jpeg',
+      extension: 'jpeg',
+    });
+
+    cover.addImage(logo, {
+      tl: { col: 0, row: 0 },
+      ext: { width: 500, height: 200 },
+    });
+
+    workbook.xlsx.writeBuffer().then((data) => {
+      const blob = new Blob([data], {
+        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      });
+      //  saveAs(blob, 'export.xlsx');
+    });
   }
 }
